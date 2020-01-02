e180a76e957305278909122c8c0934151ae4e513
Giusy <luminal@ymail.com>
2020-01-02 16:42:08 +0000

    challenge 12 done

--- jsES6/challenge_12/script.js
+++ jsES6/challenge_12/script.js
@@ -5,16 +5,23 @@ Create copy of the a array.
 
 "use strict";
 
+// var a = [1, 2, 3, [4, 5]];
 var a = [1, 2, 3];
 
 var b;
 
-//Copy array here
+// b = a.slice();
+b = [...a];
 
 b.push("newElement");
+// b[3].push(6);
+
+//Copy array here
 
 console.log(a);
+
 // [1, 2, 3]
+
 
 console.log(b);
 // [1, 2, 3, "newElement"]
