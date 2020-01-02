b71b6732f8a6cf6444e9b14ebe67a6b08130ba63
Giusy <luminal@ymail.com>
2020-01-02 17:15:54 +0000

    chellenge 15 added and solved

---
+++ jsES6/challenge_15/script.js
@@ -0,0 +1,33 @@
+/* CHALLENGE 15 - Iterate over String
+
+Count number of the lowercase vowel letters in the string.
+Vowel letters - a, e, i, o, u
+*/
+
+"use strict";
+
+var vowelsCount = 0;
+var vowels = ["a", "e", "i", "o", "u"];
+
+var str = "Today is best day of my life";
+
+// Write code here
+// for(let i = 0; i < str.length; i++ ){
+//     // if(vowels.indexOf(str[i]) !== -1)vowelsCount++;
+//     // new method ES6
+//     if(vowels.includes(str[i])) vowelsCount++;
+//     // str.charAt(i)
+// }
+//or
+
+// this cannot be used because it's a string and not an array
+// str.forEach( s => console.log(s));
+
+//or
+for(let s of str){
+    if(vowels.includes(s)) vowelsCount++;
+}
+
+
+console.log(vowelsCount);
+// 8
